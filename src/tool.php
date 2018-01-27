161d2ea75a7ef1f3ce8f8faeac7cfa8661229cae
SenaYan <qinuoyun@qq.com>
2018-01-27 22:03:32 +0800

    no message

---
+++ src/tool.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Conveyancer PHP Framework
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2016-2018 Yan TianZeng<qinuoyun@qq.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.ub-7.com
+ */
+namespace conveyancer\tool;
+/**
+ * 框架基础配置文件
+ */
+class tool {
+
+    public function __construct() {
+        # code...
+    }
+
+    public function test() {
+        echo "tool-测试" . "<br>";
+    }
+}
